2385cb3d6bed02dc28a6c6137985c9cbf658dcc2
Ironious8 <tyronekariuki@gmail.com>
2024-07-11 11:56:38 +0300

    Add find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    for(let entry of record) {
+        if(entry.result==="W") {
+          return  entry.year==="2015";
+        
+        }
+        else {
+            return undefined;
+        }
+    }
+}
+
